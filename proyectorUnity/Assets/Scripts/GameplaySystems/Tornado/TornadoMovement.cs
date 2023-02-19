361d304b166c0fc02d77b41e2c055b7993277b37
SerONaSer <123658622+SerONaSer@users.noreply.github.com>
2023-02-19 13:44:27 +0100

    Comentar
    He comentado el commit anterior perdón por no hacerlo a la vez

--- proyectorUnity/Assets/Scripts/GameplaySystems/Tornado/TornadoMovement.cs
+++ proyectorUnity/Assets/Scripts/GameplaySystems/Tornado/TornadoMovement.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class TornadoMovement : MonoBehaviour
 {
+    /// <summary>
+    /// GameObject padre con los puntos por donde pasará el tornado
+    /// </summary>
     [SerializeField]
     GameObject _tornadoPositions;
-    [SerializeField]
-    float _tornadoSpeed;
-    Vector3 _startpos;
+    /// <summary>
+    /// ID del punto al que irá el tornado
+    /// </summary>
     [SerializeField]
     int id = 0;
+    /// <summary>
+    /// Velocidad del tornado
+    /// </summary>
+    [SerializeField]
+    float _tornadoSpeed;
+    /// <summary>
+    /// Posición inicial del tornado
+    /// </summary>
+    Vector3 _startpos;
 
     private void Start()
     {
-        _startpos = transform.position;
+        _startpos = transform.position; //Guarda la posición inicial del tornado, ya que depende de cada nivel
     }
 
     private void Update()
     {
-        if (id < _tornadoPositions.transform.childCount)
+        if (id < _tornadoPositions.transform.childCount)//Ha pasado el tornado por todos los puntos?
         {
+            //almacena la dirección sin normalizar entre el inicio y el fin del movimiento
             Vector2 dir = transform.position - _tornadoPositions.transform.GetChild(id).transform.position;
+            //Mueve el tornado
             transform.position = Vector2.MoveTowards(transform.position, _tornadoPositions.transform.GetChild(id).transform.position, _tornadoSpeed * Time.deltaTime);
-            print("WWWW");
-            if (dir.magnitude < 0.5f)
+
+            if (dir.magnitude < 0.5f)//Pasa al siguiente punto del movimiento
             {
                 id++;
             }
         }
-        else if((transform.position-_startpos).magnitude > 0.5f)
+        else if((transform.position-_startpos).magnitude > 0.5f) //Volver a la posición inicial
         {
             transform.position = Vector2.MoveTowards(transform.position, _startpos, _tornadoSpeed * Time.deltaTime);
         }
-        else
+        else //Si ha vuelto a la posición inicial se destruye
         {
             Destroy(gameObject);
         }
