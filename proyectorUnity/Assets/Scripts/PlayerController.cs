d625799ac8d043cf8f74e1e5593b09d80ffd6e11
SerONaSer <andrga37@ucm.es>
2023-03-05 14:52:40 +0100

    Animaciones Charlie, y ajustes

--- proyectorUnity/Assets/Scripts/PlayerController.cs
+++ proyectorUnity/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     public bool _irACasa;
     [SerializeField] public Transform _casa;
+    Animator _anim;
+    Rigidbody2D _rb;
     
     /// <summary>
     /// Instancia del PlayerController
@@ -17,8 +19,18 @@ public class PlayerController : MonoBehaviour
     {
         Instance = this;
     }
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
+        _anim.SetFloat("Horizontal", Input.GetAxisRaw("Horizontal"));
+        _anim.SetFloat("Vertical", Input.GetAxisRaw("Vertical"));
+        _anim.SetBool("Walk", Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f || Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f);
+
         if (_irACasa) //(&& _casa.position - transform.position).magnitude > 0.5)
         {
             GetComponent<MovementController>().Stop();
