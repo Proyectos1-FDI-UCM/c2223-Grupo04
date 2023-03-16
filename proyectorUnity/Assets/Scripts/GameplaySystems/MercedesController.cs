7f95569e0083aab73c75c7982c7dacccf74c577b
Diego Montenegro <dmonoficial22@gmail.com>
2023-03-16 18:33:00 +0100

    mercedes funcionando!

--- proyectorUnity/Assets/Scripts/GameplaySystems/MercedesController.cs
+++ proyectorUnity/Assets/Scripts/GameplaySystems/MercedesController.cs
@@ -7,13 +7,18 @@ public class MercedesController : MonoBehaviour
     LevelManager levelManager;
     [SerializeField]
     float minTimeForEating, maxTimeForEating;
+    [SerializeField]
+    float mercedesSpeed, tiempoEsperaComidaCancelada;
     float timeForEating;
-
+    Transform transformPlanta;
+    PlantaBehaviour planta;
+    [SerializeField]
     MercheStates estado;
     enum MercheStates
     {
         Comiendo,
-        Esperando
+        Esperando,
+        Desplazandose
     }
     void Start()
     {
@@ -24,32 +29,64 @@ public class MercedesController : MonoBehaviour
 
     void Update()
     {
-        if(estado == MercheStates.Esperando)
-            timeForEating -= Time.deltaTime;
-        if(timeForEating < 0)
-        {
-            estado = MercheStates.Comiendo;
-            Comer();
-        }
-    }
 
-    private void Comer()
+        if (estado == MercheStates.Esperando)
+        {
+            timeForEating -= Time.deltaTime;
+            if (timeForEating < 0)
+                MoverAComer();
+        }
+        if (estado == MercheStates.Desplazandose)
+        {
+            if (planta != null)
+            {
+                Vector2 direccion = transform.position - transformPlanta.position;
+                transform.position = Vector2.MoveTowards(transform.position, transformPlanta.position, mercedesSpeed * Time.deltaTime);
+                if (direccion.magnitude < 0.5)
+                    Comer();
+            } else
+            {
+                timeForEating = tiempoEsperaComidaCancelada;
+                estado = MercheStates.Esperando;
+            }
+        }
+        
+    }
+    /// <summary>
+    /// Método que "llama a comer" a merche, haciendo que comience a moverse hacia una planta para comersela.
+    /// </summary>
+    private void MoverAComer()
     {
-        //Programar la animación a ejecutar mientras está comiendo. La animación tomará un tiempo.
-        PlantaBehaviour planta = levelManager.GetGrownPlant();
+        planta = levelManager.GetGrownPlant();
         if (planta != null)
         {
-            Transform transformPlanta = planta.transform;
-            transform.position = transformPlanta.position;
-            planta.RemovePlant();
-            Debug.Log("A comer!");
+            transformPlanta = planta.transform;
+            estado = MercheStates.Desplazandose;
         }
-        GenerateEatTime();
-        estado = MercheStates.Esperando;
+        else
+        {
+            GenerateEatTime();
+        }
+
     }
 
+    /// <summary>
+    /// Las acciones a realizar cuando merche se come una planta.
+    /// </summary>
+    private void Comer()
+    {
+        planta.RemovePlant();
+        estado = MercheStates.Esperando;
+        GenerateEatTime();
+    }
+
+    /// <summary>
+    /// Genera un tiempo para volver a comer una planta.
+    /// </summary>
     private void GenerateEatTime()
     {
         timeForEating = Random.Range(minTimeForEating, maxTimeForEating);
     }
+
+
 }
