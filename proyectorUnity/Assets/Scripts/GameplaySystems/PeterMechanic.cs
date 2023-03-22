fca2f9bb5c2444b7c646317b82bf06316064dd37
Paigro <119442055+Paigro@users.noreply.github.com>
2023-03-22 18:12:11 +0100

    Mecánica Peter no muy hecha
    Para Andrés de mí

--- proyectorUnity/Assets/Scripts/GameplaySystems/PeterMechanic.cs
+++ proyectorUnity/Assets/Scripts/GameplaySystems/PeterMechanic.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class PeterMechanic : MonoBehaviour
 {
-    [SerializeField]
-    float time;
-    [SerializeField]
-    Transform _semillaSpawn;
-    [SerializeField]
-    GameObject _semillaDestructor;
+    public GameObject[] semillas; // Array de los elementos que puede lanzar Peter al río.
+    [SerializeField] float time; // Tiempo entre cada elemento lanzado por Peter.
+    [SerializeField] Transform _semillaSpawn; // Lugar donde aparecen las semillas lanzadas.
+    [SerializeField] GameObject _semillaDestructor; //Lugar donde se destruyen las semillas
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = _semillaSpawn.position;
-
-     
+        //transform.position = _semillaSpawn.position;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(collision.gameObject); 
+        Debug.Log("ColisiónUwU");
+        if (collision.gameObject.transform.position.x < _semillaDestructor.gameObject.transform.position.x) 
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
