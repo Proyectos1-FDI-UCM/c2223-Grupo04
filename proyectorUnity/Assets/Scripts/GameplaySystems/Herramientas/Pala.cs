61cc15bb42621db37d9c76106078ccbbdda1bb5b
Andrés Marín Martínez <anmari17@ucm.es>
2023-03-27 21:07:20 +0200

    Actualización de disposición de elementos y dificultad de tornados + Animación de regadera choca
    Cambios en la disposición de elementos del nivel 2 y 3.
    Creación de la animación pala choca.

--- proyectorUnity/Assets/Scripts/GameplaySystems/Herramientas/Pala.cs
+++ proyectorUnity/Assets/Scripts/GameplaySystems/Herramientas/Pala.cs
@@ -5,6 +5,7 @@ public class Pala : Tool
 {
     [SerializeField]
     GameObject _palaAnim;
+    private GameObject _animation;
 
     public override void OnClickFunction(GameObject objetoClicado, InventoryController inventoryController)
     {
@@ -13,18 +14,25 @@ public class Pala : Tool
         {
             if (!objetoClicado.GetComponent<SoilComponent>().IsEmpty())
             {
-                GameObject.Instantiate(_palaAnim, objetoClicado.transform);
+                _animation = GameObject.Instantiate(_palaAnim, objetoClicado.transform);
+                _animation.GetComponent<Animator>().SetTrigger("Accionar");
             }
             objetoClicado.GetComponent<SoilComponent>().RemovePlant();
-        } else if(objetoClicado.GetComponent<ObstacleBehaviour>() != null)
+        } else if (objetoClicado.GetComponent<ObstacleBehaviour>() != null)
         {
-            GameObject.Instantiate(_palaAnim, objetoClicado.transform);
+            _animation = GameObject.Instantiate(_palaAnim, objetoClicado.transform);
+            _animation.GetComponent<Animator>().SetTrigger("Accionar");
             objetoClicado.GetComponent<ObstacleBehaviour>().RemoveObstacle(.75f);
-        } else if(objetoClicado.GetComponent<MercedesController>() != null)
+        }else if (objetoClicado.tag == "Obstaculo") 
+        {
+            _animation = GameObject.Instantiate(_palaAnim, objetoClicado.transform);
+            _animation.GetComponent<Animator>().SetTrigger("Chocar");
+        } 
+        else if(objetoClicado.GetComponent<MercedesController>() != null)
         {
             objetoClicado.GetComponent<MercedesController>().Stunear();
             Debug.Log("mercedes stuneada");
-        }
+        } 
 
     }
 }
