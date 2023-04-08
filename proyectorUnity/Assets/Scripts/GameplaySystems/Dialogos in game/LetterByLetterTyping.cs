61a104a62f3766e2aa939932c850e6bbcb1dac3a
Andrés Marín Martínez <anmari17@ucm.es>
2023-04-08 22:42:57 +0200

    Inicio de la implementación del sistema de sonidos
    - Por ahora solo ejecuta un sonido
    - Como estoy jugando con el tema de los tiempos, está un poco roto y no termina de ejecutarse cuando yo quiero.
    - A veces no se ejecuta.
    
    HAY QUE REVISAR

--- proyectorUnity/Assets/Scripts/GameplaySystems/Dialogos in game/LetterByLetterTyping.cs
+++ proyectorUnity/Assets/Scripts/GameplaySystems/Dialogos in game/LetterByLetterTyping.cs
@@ -10,8 +10,16 @@ public class LetterByLetterTyping : MonoBehaviour
     float _delay;
     float _delayReset;
     TextMeshProUGUI _dialogos;
-    string _dialogue;
+    string _dialogue = "";
     int _controlVar = 0;
+    
+    //SOUND
+    [SerializeField]
+    private AudioClip _dialogueTypingSounds;
+    [SerializeField]
+    private bool _stopAudioSource;
+
+    private AudioSource _audioTrack;
 
 
     // Update is called once per frame
@@ -19,12 +27,15 @@ public class LetterByLetterTyping : MonoBehaviour
     {
 
         if (_controlVar < _dialogue.Length) 
-        { 
+        {
+            
             if (_delay <= 0f) 
             {
+                PlayDialogueSound(_dialogue.Length);
                 _dialogos.text += _dialogue[_controlVar];
+                
                 _controlVar++;
-             _delay = _delayReset;
+                _delay = _delayReset;
             }
             else _delay -= Time.deltaTime;
         }
@@ -44,5 +55,18 @@ public class LetterByLetterTyping : MonoBehaviour
     {
         _controlVar = 0;
         _dialogue = ""; 
+    }
+    
+    private void PlayDialogueSound(int currentDisplayedCharCount) 
+    {
+        if(currentDisplayedCharCount % 3 == 0)
+        {
+            _audioTrack = this.gameObject.AddComponent<AudioSource>();
+            if (_stopAudioSource)
+            {
+                _audioTrack.Stop();
+            }
+            _audioTrack.PlayOneShot(_dialogueTypingSounds);
+        } 
     }
 }
