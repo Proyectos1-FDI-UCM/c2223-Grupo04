d6c894ade4d128c51c6702b520bad16efe4beda0
Diego Montenegro Calvo <73708155+guco23@users.noreply.github.com>
2023-04-21 09:51:57 +0200

    empezando el tema de animacion aparecer invernadero

---
+++ proyectorUnity/Assets/Scripts/GameFeel/InvernaderoMaker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvernaderoMaker : MonoBehaviour
+{
+    GameObject[] _invernaderos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int c = transform.childCount;
+        for (int i = 0; i < c; i++)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
